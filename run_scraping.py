5deea1668722fe638be26a95c495c77787aaf346
Mercyplus <sobachka1966@mail.ru>
2020-12-10 15:07:16 +0300

    изменение в коде

--- run_scraping.py
+++ run_scraping.py
@@ -31,18 +31,16 @@ def get_settings():
 
 
 def get_urls(_settings):
-    qs = Url.objects.all().values()
-    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    qs = Url.objects.all()
+    url_dct = {(q.city_id, q.language_id): q.url_data for q in qs}
     urls = []
     for pair in _settings:
-        if pair in url_dict:
+        if pair in url_dct:
             tmp = {}
             tmp['city'] = pair[0]
             tmp['language'] = pair[1]
-            url_data = url_dict.get(pair)
-            if url_data:
-                tmp['url_data'] = url_dict.get(pair)
-                urls.append(tmp)
+            tmp['url_data'] = url_dct[pair]
+            urls.append(tmp)
     return urls
 
 
